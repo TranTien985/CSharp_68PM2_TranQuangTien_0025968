ae437c082c679eb4ccee90011dc770bf84729f83
Tien Tran <tranqtiennd2005@gmail.com>
2026-06-17 12:18:00 +0700

    feat<sinhvien>: SearchPaging

--- UC_QLSV.cs
+++ UC_QLSV.cs
@@ -124,6 +124,47 @@ namespace CSharp_68PM2_TranQuangTien_0025968
             }
         }
 
+        private void btnLamMoi_Click(object sender, EventArgs e)
+        {
+            txtMaSV.Clear();
+            txtHoTen.Clear();
+            txtTimKiem.Clear();
+            dtpNgaySinh.Value = DateTime.Now;
+            if (cboGioiTinh.Items.Count > 0) cboGioiTinh.SelectedIndex = 0;
+            if (cboLop.Items.Count > 0) cboLop.SelectedIndex = 0;
+            txtMaSV.Focus();
+            LoadData();
+        }
+
+        private void ThucHienTimKiem()
+        {
+            string tuKhoa = txtTimKiem.Text.Trim();
+            db = new dbDataContext();
+
+            var ketQua = from sv in db.SinhViens
+                         where sv.MaSV.Contains(tuKhoa) || sv.HoTen.Contains(tuKhoa) || sv.MaLop.Contains(tuKhoa)
+                         select new
+                         {
+                             MaSV = sv.MaSV,
+                             HoTen = sv.HoTen,
+                             NgaySinh = sv.NgaySinh,
+                             GioiTinh = sv.GioiTinh,
+                             MaLop = sv.MaLop
+                         };
+
+            dgvSinhVien.DataSource = ketQua.ToList();
+        }
+
+        private void btnTim_Click(object sender, EventArgs e)
+        {
+            ThucHienTimKiem();
+        }
+
+        private void txtTimKiem_TextChanged(object sender, EventArgs e)
+        {
+            ThucHienTimKiem();
+        }
+
         private void dgvSinhVien_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             if (e.RowIndex >= 0)
